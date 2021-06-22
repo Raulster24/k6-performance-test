9b011c75e69e1bfbc205ddc4947e0419920dd6f1
rahul.srivastava <rahul.srivastava@g42.ai>
2021-06-22 15:02:29 +0400

    [feat] : added simple script

---
+++ tests/rest_get.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export let options = {
+  vus: 10,
+  duration: '30s',
+};
+export default function () {
+  http.get('https://reqres.in/api/users?page=2');
+  sleep(1);
+}
